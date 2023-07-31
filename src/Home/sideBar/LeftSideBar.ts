b089365cd1749e81b100e70d71275fde9c7e79df
hermeson <hermesondovalebeserra722@gmail.com>
2023-07-31 17:04:24 -0300

    change the first part of side bar

--- src/Home/sideBar/LeftSideBar.ts
+++ src/Home/sideBar/LeftSideBar.ts
@@ -4,10 +4,15 @@ import styled from 'styled-components'
 export const StyledLeftDiv = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 0.94rem;
   margin-left: 6.12rem;
-  align-items: center;
-  margin-right: 1.18rem;
+  padding-right: 1.18rem;
+
+  border-right: 1px solid ${(props) => props.theme.Dark7};
+`
+
+export const StyledProfile = styled(StyledLeftDiv)`
+  justify-content: flex-end;
+  height: 15%;
 `
 
 export const StyledA = styled.a`
@@ -30,4 +35,14 @@ export const StyledButtonTweet = styled.a`
   &:hover {
     background: ${(props) => props.theme['Dark-Primary-Blue']};
   }
+`
+
+export const StyledP = styled.p`
+  font-size: 1rem;
+  font-weight: 700;
+`
+
+export const StyledUserP = styled(StyledP)`
+  font-weight: 400;
+  color: ${(props) => props.theme.Dark5};
 `
--- src/Home/sideBar/LeftSideBar.tsx
+++ src/Home/sideBar/LeftSideBar.tsx
@@ -7,67 +7,90 @@ import {
   CiViewList,
   CiHashtag,
 } from 'react-icons/ci'
-import { StyledLeftDiv, StyledA, StyledButtonTweet } from './LeftSideBar.ts'
+import { FiMoreHorizontal } from 'react-icons/fi'
+import {
+  StyledLeftDiv,
+  StyledA,
+  StyledButtonTweet,
+  StyledProfile,
+  StyledUserP,
+  StyledP,
+} from './LeftSideBar.ts'
 import { theme } from '../../../defaultThemes'
 
 export function LeftSideBar() {
   return (
-    <StyledLeftDiv>
-      <ul>
-        <li>
-          <FaTwitter size={28.4} color={theme['Primary-Blue']} />
-        </li>
-        <li>
-          <StyledA href="#">
-            <FaHome size={28.4} />
-            Home
-          </StyledA>
-        </li>
-        <li>
-          <StyledA href="#">
-            {' '}
-            <CiHashtag size={28.4} />
-            Explore
-          </StyledA>
-        </li>
-        <li>
-          <StyledA href="#">
-            {' '}
-            <CiMail size={28.4} />
-            Messages
-          </StyledA>
-        </li>
-        <li>
-          <StyledA href="#">
-            {' '}
-            <BsBookmark size={28.4} />
-            Bookmark
-          </StyledA>
-        </li>
-        <li>
-          <StyledA href="#">
-            {' '}
-            <CiViewList size={28.4} />
-            List
-          </StyledA>
-        </li>
-        <li>
-          <StyledA href="#">
-            {' '}
-            <CiUser size={28.4} />
-            Profile
-          </StyledA>
-        </li>
-        <li>
-          <StyledA href="#">
-            <CiCircleMore size={28.4} />
-            More
-          </StyledA>
-        </li>
-        <div style={{ marginTop: '2rem' }}>
-          <StyledButtonTweet href="#">Tweet</StyledButtonTweet>
+    <div>
+      <StyledLeftDiv>
+        <div>
+          <ul>
+            <li>
+              <FaTwitter size={28.4} color={theme['Primary-Blue']} />
+            </li>
+            <li>
+              <StyledA href="#">
+                <FaHome size={28.4} />
+                Home
+              </StyledA>
+            </li>
+            <li>
+              <StyledA href="#">
+                <CiHashtag size={28.4} />
+                Explore
+              </StyledA>
+            </li>
+            <li>
+              <StyledA href="#">
+                <CiMail size={28.4} />
+                Messages
+              </StyledA>
+            </li>
+            <li>
+              <StyledA href="#">
+                <BsBookmark size={28.4} />
+                Bookmark
+              </StyledA>
+            </li>
+            <li>
+              <StyledA href="#">
+                <CiViewList size={28.4} />
+                List
+              </StyledA>
+            </li>
+            <li>
+              <StyledA href="#">
+                <CiUser size={28.4} />
+                Profile
+              </StyledA>
+            </li>
+            <li>
+              <StyledA href="#">
+                <CiCircleMore size={28.4} />
+                More
+              </StyledA>
+            </li>
+            <div style={{ marginTop: '2rem' }}>
+              <StyledButtonTweet href="#">Tweet</StyledButtonTweet>
+            </div>
+          </ul>
         </div>
-      </ul>
-    </StyledLeftDiv>
+      </StyledLeftDiv>
+      <StyledProfile>
+        <StyledA>
+          <img
+            src="https://avatars.githubusercontent.com/u/72263429?s=400&u=40bcc48d6b0edb21dc726fc26e5be003f3f93ac6&v=4"
+            alt=""
+            width={'39px'}
+            style={{ borderRadius: ' 99999px' }}
+          />
+          <div>
+            <StyledP>Hermeson</StyledP>
+            <StyledUserP>@hermeson833</StyledUserP>
+          </div>
+
+          <FiMoreHorizontal />
+        </StyledA>
+      </StyledProfile>
+    </div>
   )
 }
